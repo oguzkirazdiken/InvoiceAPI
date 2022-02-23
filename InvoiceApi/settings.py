eb449616635da6dfd6ead08ea60ee8ab58b7e101
oogguuzz <80089928+oogguuzz@users.noreply.github.com>
2022-02-23 23:35:32 +0300

    initial commit

--- InvoiceApi/settings.py
+++ InvoiceApi/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-h%)pu_(#8@ic==gb9+rdr-1y_+k0jert)#mhf56_8#8i4r$hi6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
